62bf588bb2894449c064c7a3c3217eacca948c9d
travis429704 <travis429704@gmail.com>
2024-03-27 15:50:16 +0700

    update compile.ts

--- wrappers/Launchpad.compile.ts
+++ wrappers/Launchpad.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/imports/stdlib.fc', 'contracts/errors.fc', 'contracts/op-code.fc', 'contracts/params.fc', 'contracts/launchpad-utils.fc', 'contracts/launchpad_wallet.fc'],
+    targets: ['contracts/imports/stdlib.fc', 'contracts/errors.fc', 'contracts/op-codes.fc', 'contracts/params.fc', 'contracts/launchpad-utils.fc', 'contracts/launchpad_wallet.fc'],
 };
